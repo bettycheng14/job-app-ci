15a948fbcc0934613f9519558796df6d18f272a2
betty cheng <yycbetty14@gmail.com>
2026-05-21 17:34:28 +1000

    add Prometheus agent

--- auth-service/src/app.js
+++ auth-service/src/app.js
@@ -1,14 +1,40 @@
 require('dotenv').config();
 const express = require('express');
+const promClient = require('prom-client');
+
 const authRoutes = require('./routes/auth');
+
+const register = new promClient.Registry();
+if (process.env.NODE_ENV !== 'test') {
+  promClient.collectDefaultMetrics({ register });
+}
+
+const httpRequestDuration = new promClient.Histogram({
+  name: 'http_request_duration_seconds',
+  help: 'Duration of HTTP requests in seconds',
+  labelNames: ['method', 'route', 'status_code'],
+  buckets: [0.05, 0.1, 0.3, 0.5, 1, 3, 5],
+  registers: [register],
+});
 
 const app = express();
 app.use(express.json());
+
+app.use((req, res, next) => {
+  const end = httpRequestDuration.startTimer();
+  res.on('finish', () => {
+    end({ method: req.method, route: req.route?.path || req.path, status_code: res.statusCode });
+  });
+  next();
+});
+
 app.use('/api/auth', authRoutes);
+
 app.get('/health', (req, res) => res.json({ status: 'ok', service: 'auth' }));
-app.get('/metrics', (req, res) => {
-  res.set('Content-Type', 'text/plain; version=0.0.4');
-  res.send('# metrics stub\n');
+
+app.get('/metrics', async (req, res) => {
+  res.set('Content-Type', register.contentType);
+  res.end(await register.metrics());
 });
 
 module.exports = app;
